34ce2ee62fcb91669fcd7aae40be3c213cf8ea65
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-04-14 04:32:19 -0400

    compare a project to projects from specific hackathon working, need prompt fixing for better accuracy

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-from utils import generate_res, get_project_title
+from utils import generate_res, get_project_title, project_gallery, compare
 
 app = Flask(__name__)
 
@@ -13,10 +13,23 @@ def hello_world():
     
     data = request.get_json()
     url = data.get("url")
+    hackathon_url = data.get("hackathon_url")
 
-    if not url:
-        return {"error": "URL is required."}, 400
+    if not url or not hackathon_url:
+        return {"error": "One URL missing"}, 400
     
     res = generate_res(url)
 
-    return {"description": res, "title": get_project_title(url)}, 200
+    comparisons = []
+
+    for project in project_gallery(hackathon_url):
+        try:
+            if not project == 0:
+                comaprison_record = compare(url, project)
+
+                if not comaprison_record == 0:
+                    comparisons.append(comaprison_record)
+        except:
+            pass
+
+    return {"description": res, "title": get_project_title(url), "comparisons": comparisons}, 200
--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,8 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
 import { Loader, Terminal } from "lucide-react";
 import React, { useState } from "react";
@@ -27,10 +29,12 @@ export default function Home() {
 	const [isAppFound, setIsAppFound] = useState(false);
 	const [Title, setTitle] = useState("");
 	const [Description, setDescription] = useState("");
+	const [Comparisons, setComparisons] = useState([]);
 
 	const [AnalyzeText, setAnalyzeText] = useState("Analyze");
 	const [isLoading, setIsLoading] = useState(false);
 
+	const [hackathonEntered, setHackathonEntered] = useState("");
 	const [urlEntered, setUrlEntered] = useState("");
 
 	const { disconnect } = useDisconnect();
@@ -43,8 +47,30 @@ export default function Home() {
 		setUrlEntered(e.target.value);
 	};
 
+	const handleHackathonChange = (e) => {
+		setHackathonEntered(e.target.value);
+	};
+
+	const AlertingMsg = (msg) => {
+		toast.error("Oops", {
+			description: msg,
+			type: "error",
+		});
+	};
+
 	const handleAnalyzeBtn = () => {
-		setAnalyzeText(<Loader className="h-4 w-4 animate-spin" />);
+		if (urlEntered === "" || hackathonEntered === "") {
+			AlertingMsg("One of the URL is missing");
+			return;
+		}
+
+		setIsLoading(true);
+		setAnalyzeText(
+			<div className="flex gap-2 items-center">
+				<Loader className="h-4 w-4 mr-2 animate-spin" />
+				<p>Loading ...</p>
+			</div>
+		);
 
 		fetch("http://127.0.0.1:5000/flask_api/summarize", {
 			method: "POST",
@@ -53,14 +79,24 @@ export default function Home() {
 			},
 			body: JSON.stringify({
 				url: urlEntered,
+				hackathon_url: hackathonEntered,
 			}),
 		})
 			.then((response) => response.json())
 			.then((data) => {
 				setAnalyzeText("Analyze");
+				setIsLoading(false);
 				setIsAppFound(true);
 				setTitle(data.title);
 				setDescription(data.description);
+
+				console.log(data.comparisons);
+				let comparisons = [];
+				for (const item of data.comparisons) {
+					comparisons.push(item);
+				}
+
+				setComparisons(comparisons);
 			})
 			.catch((error) => {
 				console.error("Error:", error);
@@ -79,40 +115,80 @@ export default function Home() {
 				</Button>
 			</nav>
 			<section className="md:w-[60%] w-full flex flex-col gap-6">
-				<div className="flex flex-col md:flex-row gap-4 md:gap-6">
-					<Input
-						placeholder="Insert the link to the project"
-						value={urlEntered}
-						onChange={handleUrlChange}
-					/>
-					<Button variant="" onClick={handleAnalyzeBtn}>
-						{AnalyzeText}
-					</Button>
-				</div>
-				{isAppFound && (
+				<section className="flex flex-col w-full gap-6">
+					<div className="flex md:flex-row flex-col gap-4 md:gap-6 w-full">
+						<Input
+							placeholder="Insert the link to the project you want to analyze"
+							value={urlEntered}
+							onChange={handleUrlChange}
+						/>
+						<Input
+							placeholder="Which Hackathon would you like it to refere to"
+							value={hackathonEntered}
+							onChange={handleHackathonChange}
+						/>
+					</div>
+					<div className="flex items-center justify-center">
+						<Button
+							onClick={handleAnalyzeBtn}
+							className="md:w-fit w-full"
+						>
+							{AnalyzeText}
+						</Button>
+					</div>
+				</section>
+				{isLoading && (
+					<Alert className="flex flex-col justify-between p-4 gap-2">
+						<Skeleton className="h-4 w-[150px]" />
+						<Skeleton className="h-4 w-full" />
+						<Skeleton className="h-4 w-full" />
+						<Skeleton className="h-4 w-[200px]" />
+					</Alert>
+				)}
+				{isAppFound && !isLoading && (
 					<Alert>
 						<AlertTitle>{Title}</AlertTitle>
 						<AlertDescription>{Description}</AlertDescription>
 					</Alert>
 				)}
 				<div className="md:grid md:grid-cols-2 flex flex-col gap-6">
-					{isAppFound && (
-						<Card className="md:h-64 flex flex-col justify-between">
-							<CardHeader>
-								<p className="text-red-400 font-semibold">
-									90% (HackPSU2024)
-								</p>
-								<CardTitle>Plagiarism</CardTitle>
-								<CardDescription>
-									An app that detects plagiarsim in projects
-								</CardDescription>
-							</CardHeader>
-							<CardFooter>
-								<Button variant="secondary" className="w-full">
-									View Comparison
-								</Button>
-							</CardFooter>
-						</Card>
+					{isLoading && <Loader className="h-6 w-6 animate-spin" />}
+					{isAppFound && !isLoading && (
+						<>
+							{Comparisons.length > 0 &&
+								Comparisons.map((comparison) => (
+									<Card
+										key={Comparisons.indexOf(comparison)}
+										className="md:h-64 flex flex-col justify-between"
+									>
+										<CardHeader>
+											<CardTitle>
+												{comparison.title}
+											</CardTitle>
+											<CardDescription className="md:overflow-scroll md:max-h-20">
+												{comparison.explanation}
+											</CardDescription>
+										</CardHeader>
+										<CardFooter>
+											<Button
+												variant="secondary"
+												className="w-full"
+												onClick={() => {
+													toast.success(
+														"Should be redirected to this link",
+														{
+															description:
+																comparison.link,
+														}
+													);
+												}}
+											>
+												View Comparison
+											</Button>
+										</CardFooter>
+									</Card>
+								))}
+						</>
 					)}
 				</div>
 			</section>
